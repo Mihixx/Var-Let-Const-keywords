0b10e514f8d7fabc9ef0aa57007a4c19572e3ba0
Mihixx <mihiranga@marketpushapps.com>
2024-09-24 11:18:22 +0530

    Add files via upload

---
+++ behind-scenes-02-block-scope/app.js
@@ -0,0 +1,25 @@
+var name = 'Max';
+let hobbies;
+
+
+{
+  let test = 5;
+  console.log(test);
+}
+
+console.log(test);
+
+if(name === 'Max'){
+ bbies = ['sports','Cooking'];
+  console.log(hobbies);
+}
+
+const person = {}
+
+function greet(){
+  var age = 30;
+  var name = 'Manuel';
+  console.log(name,age,hobbies);
+}
+console.log(name,hobbies);
+greet();
